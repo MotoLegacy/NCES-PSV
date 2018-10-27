e287af0df4384d03ed5471763213e1ebc12324ee
RGee94 <robby.gee94@gmail.com>
2018-10-27 13:44:21 -0600

    main file, graphics.h

---
+++ src/graphics.h
@@ -0,0 +1,29 @@
+#ifndef _GRAPHICS_
+#define _GRAPHICS_
+//--------------------PSP SECTION------------------------//
+#ifdef PSP
+
+#include <pspkernel.h>
+#include <oslib/oslib.h>
+
+OSL_IMAGE *defineImage(char *filename);
+
+void drawImage(OSL_IMAGE *img, int x, int y, int scalex, int scaley);
+//---------------------VITA SECTION----------------------//
+#else
+
+#include <vita2d.h>
+#include <string>
+
+vita2d_texture *defineImage(const char *filename);
+
+void drawImage(const vita2d_texture *texture, float x, float y, float scalex, float scaley);
+
+#endif
+//--------------------GENERAL SECTION-----------------------//
+void init();
+void startDrawing();
+void endDrawing();
+int syncFrame();
+
+#endif
