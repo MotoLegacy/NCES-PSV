86d377b18e11c1b6cf0a8e9b7947c254f12b1a97
RGee94 <robby.gee94@gmail.com>
2018-10-27 23:09:09 -0600

    can now ftp from make.sh, supply ip as command argument, directories for psp must exist on vita

--- make.sh
+++ make.sh
@@ -2,10 +2,16 @@
 # MakeStuff
 
 cd vita/build/
+echo -e "\n-----------------Compiling for vita--------------------------\n"
 cmake .
 make clean
 make
+curl -T NCESPSV.vpk ftp://$1:1337/ux0:/
 
 cd ../../psp/build
+echo -e "\n-----------------Compiling for psp---------------------------\n"
 make clean
 make
+cd ../
+curl -T "gfx/test.png" ftp://$1:1337/ux0:/pspemu/PSP/GAME/NCESPSP/gfx/
+curl -T "build/EBOOT.PBP" ftp://$1:1337/ux0:/pspemu/PSP/GAME/NCESPSP/
